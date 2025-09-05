de91342d6dfea4281857fca1ab9fbf35fb9dd49f
slimyzockt77 <slimyzockt77@gmail.com>
2025-09-05 17:18:31 +0200

    finding out api

--- exapmle/cttp_simple.c
+++ exapmle/cttp_simple.c
@@ -2,8 +2,20 @@
 #include "../src/cttp.c"
 #include <stdio.h>
 
+b8 index(CTTP_Respose respose, CTTP_Request request) {
+
+    return 1;
+}
+
 int main(int argc, char **argv) {
 
     printf("Hello");
+
+    CTTP_Server server = cttp_start();
+
+    cttp_handle(&server, CTTP_MethodFlag_GET, cttp_path("index","xx"), index);
+
+    cttp_end(&server);
+
     return 0;
 }
